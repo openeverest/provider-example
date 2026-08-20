432c46d88630e8ff2d2e8910a888ae53768e9006
Diogo Recharte <diogo.recharte@solanica.io>
2026-08-20 16:29:20 +0100

    chore: scaffold the project with provider-sdk init
    Unmodified output of:
    
      provider-sdk init --name provider-example \
        --module github.com/openeverest/provider-example
    
    Keeping the raw scaffold as the first commit makes the follow-up
    implementation reviewable on its own.
    
    Signed-off-by: Diogo Recharte <diogo.recharte@solanica.io>

---
+++ gen.go
@@ -0,0 +1,5 @@
+// Package providerexample contains the //go:generate directive
+// for generating the Provider CR spec for the Helm chart.
+package providerexample
+
+//go:generate go tool provider-sdk generate
---
+++ internal/common/spec.go
@@ -0,0 +1,11 @@
+// Package common defines shared constants used across the provider.
+package common
+
+const (
+	// ProviderName is the canonical name of this provider.
+	ProviderName = "provider-example"
+
+	// TODO: Define component names matching those in provider-config.yaml.
+	// Example:
+	// ComponentDatabase = "provider-example-db"
+)
